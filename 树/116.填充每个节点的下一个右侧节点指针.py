d2076145b7d124d7a90085b212495083802a320a
suhejian <hejiansu@126.com>
2022-01-12 12:39:44 +0800

    update 2022-1-12

---
+++ 树/116.填充每个节点的下一个右侧节点指针.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return 
+        queue = [root]
+        while queue:
+            n = len(queue)  # 每一层节点数
+            for i in range(n):
+                node = queue.pop(0)
+                if i < n - 1:
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
+            
