1f376c47590657d76ff1ffb31a7643df91d9a56a
suhejian <hejiansu@126.com>
2022-05-05 22:00:05 +0800

    update 2022-5-5

---
+++ 数组/滑动窗口/713.乘积小于K的子数组.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # 滑动窗口, 防止重复计算需要以右边界为准计算
+        # ABCX -> X, CX, BCX, ABCX
+        start = 0   # 滑动窗口左侧
+        res = 0 # 最终结果
+        total = 1 # 计算当前积
+        for end in range(len(nums)):
+            total *= nums[end]
+            while start <= end and total >= k:
+               total = total // nums[start]
+               start += 1
+            res += (end - start + 1)
+        return res
